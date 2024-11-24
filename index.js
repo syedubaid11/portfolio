a27fef0e1bb8987b8b85842bed61120e96a89060
syedubaid <syedabuubaid@gmail.com>
2024-11-24 22:27:21 +0530

    commit

--- index.js
+++ index.js
@@ -30,3 +30,42 @@ gsap.to(cross,{
     yPercent:100,
     ease:'none'
 })
+const hero2=document.querySelector('.hero-2');
+
+gsap.to(hero2,{
+    visibility:"hidden"
+})
+
+
+window.onload=function(){
+    const hero=document.querySelector('.hero')
+    gsap.to(hero,{
+        delay:2,
+        duration:0.7,
+        width:'100%',
+        height:'100vh',
+        fontSize:'200px',
+        ease:'power3.out',
+        backgroundImage:"url(./images/StockSnap_UV4PIZ1DEK.jpg)",
+        display:'block'
+    })
+
+
+    gsap.to(hero2,{
+        delay:2,
+        duration:1,
+        visibility:"visible"
+    })
+}
+
+
+gsap.to('.bar',1.5,{
+    delay:1.5,
+    height:0,
+    stagger:{
+        amount:0.5,
+    },
+    ease:'power4.inOut'
+
+})
+
