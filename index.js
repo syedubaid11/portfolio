883c0ae01e9a0beb1ff5aaf048370dcdc200d844
syedubaid <syedabuubaid@gmail.com>
2024-11-25 02:41:09 +0530

    commit

--- index.js
+++ index.js
@@ -13,52 +13,13 @@ gsap.ticker.add((time)=>{
 })
 
 
-//pin animation
 
-const cross=document.querySelector('.cross');
-
-gsap.to(cross,{
-    
-    scrollTrigger:{
-        trigger:'.container-hero-2',
-        start:'top top',
-        end:'bottom bottom',
-        scrub:true,
-        pin:true,
-        markers:true
-    },
-    yPercent:100,
-    ease:'none'
-})
-const hero2=document.querySelector('.hero-2');
-
-gsap.to(hero2,{
-    visibility:"hidden"
+gsap.to('.hero',1,{
+    delay:1.3,
+    ease:'power3.inOut'
 })
 
-
-window.onload=function(){
-    const hero=document.querySelector('.hero')
-    gsap.to(hero,{
-        delay:2,
-        duration:0.7,
-        width:'100%',
-        height:'100vh',
-        fontSize:'200px',
-        ease:'power3.out',
-        backgroundImage:"url(./images/StockSnap_UV4PIZ1DEK.jpg)",
-        display:'block'
-    })
-
-
-    gsap.to(hero2,{
-        delay:2,
-        duration:1,
-        visibility:"visible"
-    })
-}
-
-
+//bar animate
 gsap.to('.bar',1.5,{
     delay:1.5,
     height:0,
@@ -69,3 +30,16 @@ gsap.to('.bar',1.5,{
 
 })
 
+gsap.fromTo('.bar-text',
+{opacity:1},{ opacity:0,duration:3
+    
+})
+
+ScrollTrigger.create({
+    trigger:".cross",
+    start:'top top',
+    pin:true,
+    endTrigger:'.end-animation',
+    end:'bottom bottom',
+
+})
